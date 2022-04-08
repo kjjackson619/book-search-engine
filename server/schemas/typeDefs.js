547ab5e370dbba9ca4fac6f711eb160b8f22e5bc
KJ Jackson <kjjackson619@yahoo.com>
2022-04-08 16:21:57 -0400

    edits

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,15 +21,22 @@ link: String
 
 type Query {
     me: User
-    users: [User]
-    user(username: String!) : User
+}
+
+input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
 }
 
 type Mutation {
-    login(email: String!, password: String!) : Auth
-    addUser(username: String!, email: String!, password: String!) : Auth
-    saveBook(input: savedBooks) : User
-    removeBook(bookId: String!) : User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: String!): User
 }
 
 type Auth {
